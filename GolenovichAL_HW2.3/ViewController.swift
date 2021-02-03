f02ef1d1e36fec37b8cda1749350775d60d53327
Andrei Halianovich <igolenovichbox@gmail.com>
2021-02-03 19:22:33 +0300

    Initial Commit

---
+++ GolenovichAL_HW2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GolenovichAL_HW2.3
+//
+//  Created by Andrei Halianovich on 3.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
